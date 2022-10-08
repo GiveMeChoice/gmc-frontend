5d7dde2d79e3b8ceeb6c62d57449556937d975c1
Alex Perry <alexp3y@gmail.com>
2022-10-08 11:37:24 +0200

    Moved /src files into root of next projects and ui package

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,7 +1,12 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: [`src/**/*.{js,ts,jsx,tsx}`, '../../packages/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    'src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    '../../packages/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
