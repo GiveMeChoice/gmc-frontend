0822ea1b615fc2b332556e0bc0a96b42feab3c72
Alex Perry <alexp3y@gmail.com>
2022-11-08 21:38:45 +0000

    Adds sanity studio

---
+++ apps/prelaunch/components/Metatags.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+
+export default function Metatags({
+  title = 'Give Me Choice — Planet Conscious Shopping & Discovery',
+  description = 'The Planet is F****ed. The human species deserves a choice.',
+  url = 'https://givemechoice.com',
+  image = 'https://givemechoice.com/img/GMC_logo_comp.png',
+}) {
+  return (
+    <Head>
+      {/* <!-- Primary Meta Tags --> */}
+      <title>Give Me Choice — Planet Conscious Shopping & Discovery</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
+    </Head>
+  );
+}
---
+++ sanity-studio/README.md
@@ -0,0 +1,13 @@
+# Sanity Blogging Content Studio
+
+## **DOT ENV**
+
+- Dotenv files are environment specific (`.env.*`) using NODE_ENV
+- Fallback to `.env.development`
+
+<hr>
+
+## **Environment Variables**
+
+- SANITY_STUDIO_API_DATASET
+  - values: `test`, `production`
