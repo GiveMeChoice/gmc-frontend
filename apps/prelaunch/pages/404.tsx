d52af6a3d1b94240f0bd4d1e8deb5121b2bcfcd1
Alex Perry <alexp3y@gmail.com>
2022-11-18 10:40:12 +0000

    Change blog logo to rounded pill shape on header and 404 page

--- apps/prelaunch/pages/404.tsx
+++ apps/prelaunch/pages/404.tsx
@@ -18,13 +18,13 @@ export default function NotFound() {
             <h3 className="text-center text-4xl font-bold">Page Not Found</h3>
             <h1 className="text-center text-9xl font-bold">404</h1>
             <div className="mt-6 flex items-center justify-center">
-              <h1 className="text-center text-3xl font-bold text-gmc-surf">
+              <h1 className="text-center text-3xl font-bold text-gmc-sunset">
                 Lost? <span className="text-black">Check out the →</span>
               </h1>
               <div className="p-3">
                 <Link className="text-bold" href="/blog">
-                  <span className="inline-block rounded-md border-2 border-gmc-ocean px-2 pb-1.5 text-3xl tracking-normal hover:cursor-pointer ">
-                    bl<span className="text-gmc-surf">o</span>g
+                  <span className="inline-block rounded-3xl border-2 border-black bg-white px-2.5 pb-1.5 text-3xl tracking-normal hover:cursor-pointer hover:bg-gmc-sunset ">
+                    blog
                   </span>
                 </Link>
               </div>
--- packages/blog/components/header.tsx
+++ packages/blog/components/header.tsx
@@ -5,7 +5,7 @@ export const Header: React.FC = () => {
   return (
     <h2 className="my-2 text-right text-2xl leading-tight tracking-tight md:text-3xl md:tracking-tighter ">
       <Link href="/blog" className="">
-        <span className="inline-block rounded-lg border-2 border-black bg-white  px-2 pb-1.5 tracking-normal duration-100 hover:cursor-pointer hover:bg-gmc-sunset">
+        <span className="inline-block rounded-3xl border-2 border-black bg-white  px-3 pb-1.5 tracking-normal duration-100 hover:cursor-pointer hover:bg-gmc-sunset">
           bl<span className="text-primar">o</span>g
         </span>
       </Link>
